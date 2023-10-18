09468743385674a193eaae8ba47c8639a750ad92
gabriellevims <gabriellevi2104@gmail.com>
2023-10-18 11:30:00 -0300

    maior

---
+++ atividade09.py
@@ -0,0 +1,8 @@
+#Exercício Python 09: faça um software que verifique entre 2 numeros qual o maior
+print("Qual é o maior")
+numero1 = float(input("digite o número 1:"))
+numero2 = float(input("digite o número 2:"))
+if numero1 > numero2:
+   print(" o número 1 é o maior ")
+else:
+    print(" o numero 2 é o maior")
